dc48cdff2c0be0de7345c0558b8a865d958469ad
Madipadige <2457151@cognizant.com>
2026-01-28 15:47:37 +0530

    Adding Complete Code

---
+++ src/test/java/testCases/TC16_FilteringCoursesAndWritingCountInExcel.java
@@ -0,0 +1,20 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import pageObject.GettingCourseDetails;
+import testBase.BaseClass;
+
+import java.io.IOException;
+
+public class TC16_FilteringCoursesAndWritingCountInExcel extends BaseClass {
+    @Test
+    public void writingInExcel() throws IOException {
+        logger.info("******Starting TC16_FilteringCoursesAndWritingCountInExcel******");
+        GettingCourseDetails gcd = new GettingCourseDetails(driver);
+        logger.info("Writing language options and their course counts to Excel file");
+        gcd.clickClearAllButton();
+        gcd.WriteLanguageOptionsAndCount();
+        logger.info("******Ending TC16_FilteringCoursesAndWritingCountInExcel******");
+
+    }
+}
